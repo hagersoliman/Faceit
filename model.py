c7d7d91e009ab78f83f837d4547ef48960bfdbfb
sara-yasser <sara.yasser.kamal@gmail.com>
2021-06-05 19:03:37 +0200

    transform

--- model.py
+++ model.py
@@ -32,9 +32,8 @@ class Vgg19(nn.Module):
         input = (input - self.mean) / self.std
         next_input = input
         for i in len(self.slices):
-          next_input = self.slices[i](next_input)
-        	out.append(next_input)
-
+            next_input = self.slices[i](next_input)
+            out.append(next_input)
         return out
 
 
@@ -61,49 +60,40 @@ class Transform:
     Random tps transformation for equivariance constraints. See Sec 3.3
     """
     def __init__(self, bs, **kwargs):
+        # tensor of random numbers with mean = 0
         noise = torch.normal(mean=0, std=kwargs['sigma_affine'] * torch.ones([bs, 2, 3]))
         self.theta = noise + torch.eye(2, 3).view(1, 2, 3)
         self.bs = bs
 
         if ('sigma_tps' in kwargs) and ('points_tps' in kwargs):
             self.tps = True
-            self.control_points = make_coordinate_grid((kwargs['points_tps'], kwargs['points_tps']), type=noise.type())
-            self.control_points = self.control_points.unsqueeze(0)
-            self.control_params = torch.normal(mean=0,
-                                               std=kwargs['sigma_tps'] * torch.ones([bs, 1, kwargs['points_tps'] ** 2]))
+            self.control_points = make_coordinate_grid((kwargs['points_tps'], kwargs['points_tps']), type=noise.type()).unsqueeze(0)
+            self.control_params = torch.normal(mean=0, std=kwargs['sigma_tps'] * torch.ones([bs, 1, kwargs['points_tps'] ** 2]))
         else:
             self.tps = False
 
     def transform_frame(self, frame):
-        grid = make_coordinate_grid(frame.shape[2:], type=frame.type()).unsqueeze(0)
-        grid = grid.view(1, frame.shape[2] * frame.shape[3], 2)
+        grid = make_coordinate_grid(frame.shape[2:], type=frame.type()).unsqueeze(0).view(1, frame.shape[2] * frame.shape[3], 2)
         grid = self.warp_coordinates(grid).view(self.bs, frame.shape[2], frame.shape[3], 2)
         return F.grid_sample(frame, grid, padding_mode="reflection")
 
     def warp_coordinates(self, coordinates):
-        theta = self.theta.type(coordinates.type())
-        theta = theta.unsqueeze(1)
-        transformed = torch.matmul(theta[:, :, :, :2], coordinates.unsqueeze(-1)) + theta[:, :, :, 2:]
-        transformed = transformed.squeeze(-1)
+        theta = self.theta.type(coordinates.type()).unsqueeze(1)
+        transformed = (torch.matmul(theta[:, :, :, :2], coordinates.unsqueeze(-1)) + theta[:, :, :, 2:]).squeeze(-1)
 
         if self.tps:
             control_points = self.control_points.type(coordinates.type())
             control_params = self.control_params.type(coordinates.type())
             distances = coordinates.view(coordinates.shape[0], -1, 1, 2) - control_points.view(1, 1, -1, 2)
             distances = torch.abs(distances).sum(-1)
-
-            result = distances ** 2
-            result = result * torch.log(distances + 1e-6)
-            result = result * control_params
+            result = (distances ** 2) * torch.log(distances + 1e-6) * control_params
             result = result.sum(dim=2).view(self.bs, coordinates.shape[1], 1)
-            transformed = transformed + result
+        return transformed + result
 
-        return transformed
-
-    def jacobian(self, coordinates):
-        new_coordinates = self.warp_coordinates(coordinates)
-        grad_x = grad(new_coordinates[..., 0].sum(), coordinates, create_graph=True)
-        grad_y = grad(new_coordinates[..., 1].sum(), coordinates, create_graph=True)
+    def jacobian(self, old_coordinates):
+        coordinates = self.warp_coordinates(old_coordinates)
+        grad_x = grad(coordinates[..., 0].sum(), old_coordinates, create_graph=True)
+        grad_y = grad(coordinates[..., 1].sum(), old_coordinates, create_graph=True)
         jacobian = torch.cat([grad_x[0].unsqueeze(-2), grad_y[0].unsqueeze(-2)], dim=-2)
         return jacobian
 
