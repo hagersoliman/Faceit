4248aad62b242eb58000b1c3889d907669b8cd54
sara-yasser <sara.yasser.kamal@gmail.com>
2021-07-09 19:46:06 +0200

    imgpyrm

--- model.py
+++ model.py
@@ -2,12 +2,12 @@ from torch import nn
 import torch
 import torch.nn.functional as F
 from modules.util import AntiAliasInterpolation2d, make_coordinate_grid
-from torchvision.models import vgg19
+from torchvision import models
 import numpy as np
 from torch.autograd import grad
 from torchvision.models import vgg19
 
-class Vgg19(nn.Module):
+class Vgg19(torch.nn.Module):
     """
     Vgg19 network for perceptual loss. See Sec 3.3.
     """
@@ -62,7 +62,7 @@ class Vgg19(nn.Module):
         return out
 
 
-class ImagePyramide(nn.Module):
+class ImagePyramide(torch.nn.Module):
     """
     Create image pyramide for computing pyramide perceptual loss. See Sec 3.3
     """
@@ -77,8 +77,8 @@ class ImagePyramide(nn.Module):
         predicted = {}
         for lvl, down_module in self.scale_down.items():
             predicted['prediction_' + str(lvl).translate({ord('.'): None})] = down_module(x)
-        # print("predicted :............")
-        # print(predicted)
+        print("out dict :............")
+        print(predicted)
         return predicted
 
 
@@ -87,40 +87,49 @@ class Transform:
     Random tps transformation for equivariance constraints. See Sec 3.3
     """
     def __init__(self, bs, **kwargs):
-        # tensor of random numbers with mean = 0
         noise = torch.normal(mean=0, std=kwargs['sigma_affine'] * torch.ones([bs, 2, 3]))
         self.theta = noise + torch.eye(2, 3).view(1, 2, 3)
         self.bs = bs
 
         if ('sigma_tps' in kwargs) and ('points_tps' in kwargs):
             self.tps = True
-            self.control_points = make_coordinate_grid((kwargs['points_tps'], kwargs['points_tps']), type=noise.type()).unsqueeze(0)
-            self.control_params = torch.normal(mean=0, std=kwargs['sigma_tps'] * torch.ones([bs, 1, kwargs['points_tps'] ** 2]))
+            self.control_points = make_coordinate_grid((kwargs['points_tps'], kwargs['points_tps']), type=noise.type())
+            self.control_points = self.control_points.unsqueeze(0)
+            self.control_params = torch.normal(mean=0,
+                                               std=kwargs['sigma_tps'] * torch.ones([bs, 1, kwargs['points_tps'] ** 2]))
         else:
             self.tps = False
 
     def transform_frame(self, frame):
-        grid = make_coordinate_grid(frame.shape[2:], type=frame.type()).unsqueeze(0).view(1, frame.shape[2] * frame.shape[3], 2)
+        grid = make_coordinate_grid(frame.shape[2:], type=frame.type()).unsqueeze(0)
+        grid = grid.view(1, frame.shape[2] * frame.shape[3], 2)
         grid = self.warp_coordinates(grid).view(self.bs, frame.shape[2], frame.shape[3], 2)
         return F.grid_sample(frame, grid, padding_mode="reflection")
 
     def warp_coordinates(self, coordinates):
-        theta = self.theta.type(coordinates.type()).unsqueeze(1)
-        transformed = (torch.matmul(theta[:, :, :, :2], coordinates.unsqueeze(-1)) + theta[:, :, :, 2:]).squeeze(-1)
+        theta = self.theta.type(coordinates.type())
+        theta = theta.unsqueeze(1)
+        transformed = torch.matmul(theta[:, :, :, :2], coordinates.unsqueeze(-1)) + theta[:, :, :, 2:]
+        transformed = transformed.squeeze(-1)
 
         if self.tps:
             control_points = self.control_points.type(coordinates.type())
             control_params = self.control_params.type(coordinates.type())
             distances = coordinates.view(coordinates.shape[0], -1, 1, 2) - control_points.view(1, 1, -1, 2)
             distances = torch.abs(distances).sum(-1)
-            result = (distances ** 2) * torch.log(distances + 1e-6) * control_params
-            result = result.sum(dim=2).view(self.bs, coordinates.shape[1], 1)
-        return transformed + result
 
-    def jacobian(self, old_coordinates):
-        coordinates = self.warp_coordinates(old_coordinates)
-        grad_x = grad(coordinates[..., 0].sum(), old_coordinates, create_graph=True)
-        grad_y = grad(coordinates[..., 1].sum(), old_coordinates, create_graph=True)
+            result = distances ** 2
+            result = result * torch.log(distances + 1e-6)
+            result = result * control_params
+            result = result.sum(dim=2).view(self.bs, coordinates.shape[1], 1)
+            transformed = transformed + result
+
+        return transformed
+
+    def jacobian(self, coordinates):
+        new_coordinates = self.warp_coordinates(coordinates)
+        grad_x = grad(new_coordinates[..., 0].sum(), coordinates, create_graph=True)
+        grad_y = grad(new_coordinates[..., 1].sum(), coordinates, create_graph=True)
         jacobian = torch.cat([grad_x[0].unsqueeze(-2), grad_y[0].unsqueeze(-2)], dim=-2)
         return jacobian
 
@@ -134,24 +143,24 @@ class GeneratorFullModel(torch.nn.Module):
     Merge all generator related updates into single model for better multi-gpu usage
     """
 
-    def __init__(self, generator, discriminator, kp_extractor, train_params): # order changed
-        #super(GeneratorFullModel, self).__init__()
-        self.generator, self.discriminator = generator, discriminator
-        self.kp_extractor, self.train_params = kp_extractor, train_params
-
-        """self.pyramid = ImagePyramide(train_params['scales'], generator.num_channels)
-    		
+    def __init__(self, kp_extractor, generator, discriminator, train_params):
+        super(GeneratorFullModel, self).__init__()
+        self.kp_extractor = kp_extractor
+        self.generator = generator
+        self.discriminator = discriminator
+        self.train_params = train_params
+        self.scales = train_params['scales']
+        self.disc_scales = self.discriminator.scales
+        self.pyramid = ImagePyramide(self.scales, generator.num_channels)
         if torch.cuda.is_available():
-            self.pyramid = self.pyramid.cuda()"""
-        
-        self.pyramid = self.pyramid.cuda() if torch.cuda.is_available() else ImagePyramide(train_params['scales'], generator.num_channels)
-    		
+            self.pyramid = self.pyramid.cuda()
 
         self.loss_weights = train_params['loss_weights']
 
-        self.vgg = Vgg19() if sum(self.loss_weights['perceptual']) != 0 else False
-        if self.vgg and torch.cuda.is_available():
-            self.vgg = self.vgg.cuda()
+        if sum(self.loss_weights['perceptual']) != 0:
+            self.vgg = Vgg19()
+            if torch.cuda.is_available():
+                self.vgg = self.vgg.cuda()
 
     def forward(self, x):
         kp_source = self.kp_extractor(x['source'])
@@ -162,21 +171,25 @@ class GeneratorFullModel(torch.nn.Module):
 
         loss_values = {}
 
-        pyramide_real, pyramide_generated = self.pyramid(x['driving']), self.pyramid(generated['prediction'])
+        pyramide_real = self.pyramid(x['driving'])
+        pyramide_generated = self.pyramid(generated['prediction'])
 
-        if self.vgg:
+        if sum(self.loss_weights['perceptual']) != 0:
             value_total = 0
-            for scale in train_params['scales']:
-                x_vgg, y_vgg = self.vgg(pyramide_generated['prediction_' + str(scale)]), self.vgg(pyramide_real['prediction_' + str(scale)])
+            for scale in self.scales:
+                x_vgg = self.vgg(pyramide_generated['prediction_' + str(scale)])
+                y_vgg = self.vgg(pyramide_real['prediction_' + str(scale)])
+
                 for i, weight in enumerate(self.loss_weights['perceptual']):
-                    value_total += (weight * torch.abs(x_vgg[i] - y_vgg[i].detach()).mean())
+                    value = torch.abs(x_vgg[i] - y_vgg[i].detach()).mean()
+                    value_total += self.loss_weights['perceptual'][i] * value
                 loss_values['perceptual'] = value_total
 
         if self.loss_weights['generator_gan'] != 0:
             discriminator_maps_generated = self.discriminator(pyramide_generated, kp=detach_kp(kp_driving))
             discriminator_maps_real = self.discriminator(pyramide_real, kp=detach_kp(kp_driving))
             value_total = 0
-            for scale in self.discriminator.scales:
+            for scale in self.disc_scales:
                 key = 'prediction_map_%s' % scale
                 value = ((1 - discriminator_maps_generated[key]) ** 2).mean()
                 value_total += self.loss_weights['generator_gan'] * value
@@ -184,7 +197,7 @@ class GeneratorFullModel(torch.nn.Module):
 
             if sum(self.loss_weights['feature_matching']) != 0:
                 value_total = 0
-                for scale in self.discriminator.scales:
+                for scale in self.disc_scales:
                     key = 'feature_maps_%s' % scale
                     for i, (a, b) in enumerate(zip(discriminator_maps_real[key], discriminator_maps_generated[key])):
                         if self.loss_weights['feature_matching'][i] == 0:
@@ -195,9 +208,10 @@ class GeneratorFullModel(torch.nn.Module):
 
         if (self.loss_weights['equivariance_value'] + self.loss_weights['equivariance_jacobian']) != 0:
             transform = Transform(x['driving'].shape[0], **self.train_params['transform_params'])
-            transformed_kp = self.kp_extractor(transform.transform_frame(x['driving']))
+            transformed_frame = transform.transform_frame(x['driving'])
+            transformed_kp = self.kp_extractor(transformed_frame)
 
-            generated['transformed_frame'] = transform.transform_frame(x['driving'])
+            generated['transformed_frame'] = transformed_frame
             generated['transformed_kp'] = transformed_kp
 
             ## Value loss part
@@ -207,15 +221,12 @@ class GeneratorFullModel(torch.nn.Module):
 
             ## jacobian loss part
             if self.loss_weights['equivariance_jacobian'] != 0:
-                """jacobian_transformed = torch.matmul(transform.jacobian(transformed_kp['value']), 
-                																					transformed_kp['jacobian'])
+                jacobian_transformed = torch.matmul(transform.jacobian(transformed_kp['value']),
+                                                    transformed_kp['jacobian'])
 
                 normed_driving = torch.inverse(kp_driving['jacobian'])
                 normed_transformed = jacobian_transformed
-                value = torch.matmul(normed_driving, normed_transformed)"""
-
-                normed_driving = torch.inverse(kp_driving['jacobian'])
-                value = torch.matmul(normed_driving, torch.matmul(transform.jacobian(transformed_kp['value']), transformed_kp['jacobian']))
+                value = torch.matmul(normed_driving, normed_transformed)
 
                 eye = torch.eye(2).view(1, 1, 2, 2).type(value.type())
 
@@ -231,31 +242,32 @@ class DiscriminatorFullModel(torch.nn.Module):
     """
 
     def __init__(self, kp_extractor, generator, discriminator, train_params):
-        #super(DiscriminatorFullModel, self).__init__()
-        self.generator, self.discriminator = generator, discriminator
-        self.kp_extractor, self.train_params = kp_extractor, train_params
-        
-        """self.pyramid = ImagePyramide(self.discriminator.scales, generator.num_channels)
+        super(DiscriminatorFullModel, self).__init__()
+        self.kp_extractor = kp_extractor
+        self.generator = generator
+        self.discriminator = discriminator
+        self.train_params = train_params
+        self.scales = self.discriminator.scales
+        self.pyramid = ImagePyramide(self.scales, generator.num_channels)
         if torch.cuda.is_available():
-            self.pyramid = self.pyramid.cuda()"""
-        
-        self.pyramid = self.pyramid.cuda() if torch.cuda.is_available() else ImagePyramide(self.discriminator.scales, generator.num_channels)
+            self.pyramid = self.pyramid.cuda()
 
         self.loss_weights = train_params['loss_weights']
 
     def forward(self, x, generated):
+        pyramide_real = self.pyramid(x['driving'])
+        pyramide_generated = self.pyramid(generated['prediction'].detach())
+
         kp_driving = generated['kp_driving']
-        discriminator_maps_generated = self.discriminator(self.pyramid(generated['prediction'].detach()), kp=detach_kp(kp_driving))
-        discriminator_maps_real = self.discriminator(self.pyramid(x['driving']), kp=detach_kp(kp_driving))
+        discriminator_maps_generated = self.discriminator(pyramide_generated, kp=detach_kp(kp_driving))
+        discriminator_maps_real = self.discriminator(pyramide_real, kp=detach_kp(kp_driving))
 
         loss_values = {}
         value_total = 0
-        for scale in self.discriminator.scales:
+        for scale in self.scales:
             key = 'prediction_map_%s' % scale
             value = (1 - discriminator_maps_real[key]) ** 2 + discriminator_maps_generated[key] ** 2
             value_total += self.loss_weights['discriminator_gan'] * value.mean()
         loss_values['disc_gan'] = value_total
 
         return loss_values
-      
-      
