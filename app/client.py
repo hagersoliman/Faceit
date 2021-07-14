4fb6ddf5de271afb6da6474dd21c8d25f95bfba6
sara-yasser <sara.yasser.kamal@gmail.com>
2021-07-15 00:23:14 +0200

    buffering and header

--- app/client.py
+++ app/client.py
@@ -1,12 +1,27 @@
 import socket
+
+HEADER_SIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+s.connect((socket.gethostname(), 1236))
 
-full_msg = ''
+
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16) # more than header size to ensure the header is recvd
+        if new_msg:
+            print(f"new message length: {msg[:HEADER_SIZE]}")
+            msg_len = int(msg[:HEADER_SIZE]) # only header from the beginning to index 10
+            new_msg = False
 
-print(full_msg)
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg)-HEADER_SIZE == msg_len:
+            print("full msg recvd")
+            print(full_msg[HEADER_SIZE:]) # print only msg from 10 to the end
+            new_msg = True
+            full_msg = ''
+
+    print(full_msg)
--- app/server.py
+++ app/server.py
@@ -1,11 +1,23 @@
 import socket
+import time
+
+HEADER_SIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
+s.bind((socket.gethostname(), 1236))
 s.listen(5)
 
 while True:
     client_socket, address = s.accept()
     print(f"connect feom {address} has been established")
-    client_socket.send(bytes("welcome to the server", "utf-8"))
-    client_socket.close()
+
+    msg = "welcome to the server"
+    msg = f'{len(msg):<{HEADER_SIZE}}' + msg
+    client_socket.send(bytes(msg, "utf-8"))
+
+    # the folloing lines to test time
+    while True:
+        time.sleep(3)
+        msg = f"the time is {time.time()}"
+        msg = f'{len(msg):<{HEADER_SIZE}}' + msg
+        client_socket.send(bytes(msg, "utf-8"))
