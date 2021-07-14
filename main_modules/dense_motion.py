3a43af3e9a87186c4f7d59578714f9a54db67c7d
hagersoliman <47715508+hagersoliman@users.noreply.github.com>
2021-07-14 07:11:05 -0700

    Update dense_motion.py

--- main_modules/dense_motion.py
+++ main_modules/dense_motion.py
@@ -1,7 +1,7 @@
 from torch import nn
 import torch.nn.functional as F
 import torch
-from modules.util import Hourglass, AntiAliasInterpolation2d, make_coordinate_grid, kp2gaussian
+from modules.util import Hourglass, AntiAliasInterpolation2d, localized_heatmap, create_mesh
 
 
 class DenseMotionNetwork(nn.Module):
@@ -16,59 +16,42 @@ class DenseMotionNetwork(nn.Module):
                                    max_features=max_features, num_blocks=num_blocks)
 
         self.mask = nn.Conv2d(self.hourglass.out_filters, num_kp + 1, kernel_size=(7, 7), padding=(3, 3))
-
-        if estimate_occlusion_map:
-            self.occlusion = nn.Conv2d(self.hourglass.out_filters, 1, kernel_size=(7, 7), padding=(3, 3))
-        else:
-            self.occlusion = None
-
+        self.occlusion = nn.Conv2d(self.hourglass.out_filters, 1, kernel_size=(7, 7), padding=(3, 3))    
         self.num_kp = num_kp
         self.scale_factor = scale_factor
         self.kp_variance = kp_variance
-
-        if self.scale_factor != 1:
-            self.down = AntiAliasInterpolation2d(num_channels, self.scale_factor)
+        self.down = AntiAliasInterpolation2d(num_channels, self.scale_factor)
 
     
-    def create_heatmap(self, source_image, kp_driving, kp_source):
+    def create_heatmap(self, kp_driving, kp_source, w, h):
         """
         Eq 6. in the paper 
         """
         print("eq6")
-        spatial_size = source_image.shape[2:]
-        print(spatial_size)
-        gaussian_driving = kp2gaussian(kp_driving, spatial_size=source_image.shape[2:], kp_variance=self.kp_variance) 
-        print(gaussian_driving.shape)#torch.Size([1, 10, 64, 64])
-        gaussian_source = kp2gaussian(kp_source, spatial_size=source_image.shape[2:], kp_variance=self.kp_variance)
-        print(gaussian_source.shape) #torch.Size([1, 10, 64, 64])
-        heatmap = gaussian_driving - gaussian_source
-        print(heatmap.shape)
-
-        #adding background feature
-        zeros = torch.zeros(heatmap.shape[0], 1, heatmap.shape[2], heatmap.shape[3]).type(heatmap.type())
-        print("0",zeros.shape) #torch.Size([1, 1, 64, 64])
-        heatmap = torch.cat([zeros, heatmap], dim=1)
-        print("1",heatmap.shape) #torch.Size([1, 11, 64, 64])
-        heatmap = heatmap.unsqueeze(2)
-        print("2",heatmap.shape) #torch.Size([1, 11, 1, 64, 64])
+        bG = torch.zeros(1, 1, w, h).type(kp_driving['value'].type())
+        driving_heatmap = localized_heatmap(kp_driving, self.kp_variance, w, h) #torch.Size([1, 10, 64, 64])
+        source_heatmap = localized_heatmap(kp_source, self.kp_variance, w, h) #torch.Size([1, 10, 64, 64])
+        heatmap = driving_heatmap - source_heatmap
+        heatmap = torch.cat([bG, heatmap], dim=1).unsqueeze(2)
         return heatmap
 
-
-    def z_tdr(self, identity_grid, kp_driving, h, w, bs):
-        z = identity_grid.view(1, 1, h, w, 2)
+    def z_tdr(self, mesh, kp_driving, h, w, bs):
+        z = mesh.view(1, 1, h, w, 2)
         tdr = kp_driving['value'].view(bs, self.num_kp, 1, 1, 2)
         return z - tdr
     
     def jk(self, kp_source, kp_driving, h, w):
+        print("hey",kp_driving['jacobian'].shape)
+        print("heyy",kp_source['jacobian'].shape)
         jacobian = torch.matmul(kp_source['jacobian'], torch.inverse(kp_driving['jacobian']))
         jacobian = jacobian.unsqueeze(-3).unsqueeze(-3)
         jacobian = jacobian.repeat(1, 1, h, w, 1, 1)
         return jacobian
     
-    def jkDotZ_tdr(self, coordinate_grid, jacobian):
-        coordinate_grid = torch.matmul(jacobian, coordinate_grid.unsqueeze(-1)) # jk * (Z - TDR)
-        coordinate_grid = coordinate_grid.squeeze(-1)
-        return coordinate_grid
+    def jkDotZ_tdr( self, mesh, jacobian):
+        mesh = torch.matmul(jacobian, mesh.unsqueeze(-1)) # jk * (Z - TDR)
+        mesh = mesh.squeeze(-1)
+        return mesh
              
     
         
@@ -78,29 +61,28 @@ class DenseMotionNetwork(nn.Module):
         """
         print("Eq 4")
         bs, _, h, w = source_image.shape
-        identity_grid = make_coordinate_grid((h, w), type=kp_source['value'].type())
-        print("1",identity_grid.shape) #torch.Size([64, 64, 2])
-        coordinate_grid =  self.z_tdr(identity_grid, kp_driving,  h, w, bs)
-        print(coordinate_grid.shape)  #torch.Size([1, 10, 64, 64, 2])
-        if 'jacobian' in kp_driving:
-            
-            jacobian = self.jk(kp_source, kp_driving,  h, w)  # jk
-            print("6",jacobian.shape) #torch.Size([1, 10, 64, 64, 2, 2])
-            coordinate_grid = self.jkDotZ_tdr(coordinate_grid, jacobian)
-            print("8",coordinate_grid.shape) #torch.Size([1, 10, 64, 64, 2])
+        ref_mesh = create_mesh(w, h, kp_source['value'].type()) #torch.Size([64, 64, 2])
+        print("1",ref_mesh.shape) 
+        z_tdr =  self.z_tdr(ref_mesh, kp_driving,  h, w, bs)
+        print(z_tdr.shape)  #torch.Size([1, 10, 64, 64, 2])
+        
+        jacobian = self.jk(kp_source, kp_driving,  h, w)  # jk
+        print("6",jacobian.shape) #torch.Size([1, 10, 64, 64, 2, 2])
+        dot_out = self.jkDotZ_tdr(z_tdr, jacobian)
+        print("8",dot_out.shape) #torch.Size([1, 10, 64, 64, 2])
         
         tsr = kp_source['value'].view(bs, self.num_kp, 1, 1, 2)
-        driving_to_source = coordinate_grid + tsr #TSR + jk * (Z - TDR)
+        driving_to_source = dot_out + tsr #TSR + jk * (Z - TDR)
 
         print("9",driving_to_source.shape) #torch.Size([1, 10, 64, 64, 2])
         #adding background feature
-        identity_grid = identity_grid.view(1, 1, h, w, 2).repeat(bs, 1, 1, 1, 1)
-        print("10",identity_grid.shape) #torch.Size([1, 1, 64, 64, 2])
-        sparse_motions = torch.cat([identity_grid, driving_to_source], dim=1)
+        ref_mesh = ref_mesh.view(1, 1, h, w, 2).repeat(bs, 1, 1, 1, 1)
+        print("10",ref_mesh.shape) #torch.Size([1, 1, 64, 64, 2])
+        sparse_motions = torch.cat([ref_mesh, driving_to_source], dim=1)
         print("11",sparse_motions.shape) #torch.Size([1, 11, 64, 64, 2])
         return sparse_motions
 
-    def create_deformed_source_image(self, source_image, sparse_motions):
+    def create_wrapped_source_image(self, source_image, sparse_motions):
         """
         Eq 7. in the paper \hat{T}_{s<-d}(z)
         """
@@ -135,8 +117,8 @@ class DenseMotionNetwork(nn.Module):
         return deformation
     
     
-    def get_mask(self, heatmap_representation, deformed_source,bs,  h, w):
-        input = torch.cat([heatmap_representation, deformed_source], dim=2)
+    def get_mask(self, heatmap, wraped_source,bs,  h, w):
+        input = torch.cat([heatmap, wraped_source], dim=2)
         print("0", input.shape) # torch.Size([1, 11, 4, 64, 64])
         input = input.view(bs, -1, h, w)
         print("1", input.shape) # torch.Size([1, 44, 64, 64])
@@ -162,22 +144,16 @@ class DenseMotionNetwork(nn.Module):
         print(bs,h,w)
 
         out_dict = dict()
-        heatmap_representation = self.create_heatmap(source_image, kp_driving, kp_source)
+        heatmap = self.create_heatmap( kp_driving, kp_source,  w, h)
         sparse_motion = self.create_local_motions(source_image, kp_driving, kp_source)
-        deformed_source = self.create_deformed_source_image(source_image, sparse_motion)
-        out_dict['sparse_deformed'] = deformed_source # the source image moved due to the 
-        
-        mask , prediction = self.get_mask(heatmap_representation, deformed_source, bs,  h, w)
+        wrapped_source = self.create_wrapped_source_image(source_image, sparse_motion)
+        out_dict['sparse_deformed'] = wrapped_source # the source image moved due to the  
+        mask , prediction = self.get_mask(heatmap, wrapped_source, bs,  h, w)
         print("4", mask.shape)#torch.Size([1, 11, 64, 64])
         out_dict['mask'] = mask 
         deformation = self.get_masked_sparse_motion( mask, sparse_motion)
-       
         out_dict['deformation'] = deformation
-
-       
-        if self.occlusion:
-            occlusion_map = self.get_occlusion_map( prediction)
-            out_dict['occlusion_map'] = occlusion_map
+        occlusion_map = self.get_occlusion_map( prediction)
+        out_dict['occlusion_map'] = occlusion_map
 
         return out_dict
-    
