0ae8509074783b7870fe88bb892eabb6cbef75bb
sara-yasser <sara.yasser.kamal@gmail.com>
2021-07-15 04:05:44 +0200

    client

--- app/client.py
+++ app/client.py
@@ -1,33 +1,92 @@
 import socket
-import pickle
+import select
+import errno
+import sys
+
 
 HEADER_SIZE = 10
+IP = "127.0.0.1"
+PORT = 1234
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1237))
+my_username = input("Username: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((OP, PORT))
+client_socket.setblocking(False) # receive functionality will not be blocking
 
+username = my_username.encode("uft-8") # convert username to bytes
+username_header = f"{len(username):<{HEADER_SIZE}}".encode("utf-8")
+client_socket.send(username_header + username)
 
 while True:
-    full_msg = b'' # b for bytes
-    new_msg = True
-    while True:
-        msg = s.recv(16) # more than header size to ensure the header is recvd
-        if new_msg:
-            print(f"new message length: {msg[:HEADER_SIZE]}")
-            msg_len = int(msg[:HEADER_SIZE]) # only header from the beginning to index 10
-            new_msg = False
+    message = input(f"{my_username} > ")
 
-        # full_msg += msg.decode("utf-8")  # decode to convert from bytes to string
-        full_msg += msg  # don't need to decode as we want it bytes
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message):<{HEADER_SIZE}}".encode("utf-8")
+        client_socket.send(message_header + message)
 
-        if len(full_msg)-HEADER_SIZE == msg_len:
-            print("full msg recvd")
-            # print(full_msg[HEADER_SIZE:]) # print only msg from 10 to the end
+    try:
+        while True:
+            # receive
+            username_header = client_socket.recv(HEADER_SIZE)
+            if not len(username_header):
+                print("connection closed by server")
+                sys.exit()
 
-            d = pickle.loads(full_msg[HEADER_SIZE:]) # decode
-            print(d)
+            username_length = int(username_header.decode("utf-8").strip())
+            username = client_socket.recv(username_length).decode("utf-8")
 
-            new_msg = True
-            full_msg = b''
+            message_header = client_socket.recv(HEADER_SIZE)
+            message_length = int(message_header.decode("utf-8").strip())
+            message = client_socket.recv(message_length).decode("utf-8")
 
-    print(full_msg)
+            print(f"{username} > {message}")
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('reading error', str(e))
+            sys.exit()
+        continue
+    except:
+        print('general error', str(e))
+        sys.exit()
+
+
+
+
+
+
+
+# import socket
+# import pickle
+
+# HEADER_SIZE = 10
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((socket.gethostname(), 1237))
+
+
+# while True:
+#     full_msg = b'' # b for bytes
+#     new_msg = True
+#     while True:
+#         msg = s.recv(16) # more than header size to ensure the header is recvd
+#         if new_msg:
+#             print(f"new message length: {msg[:HEADER_SIZE]}")
+#             msg_len = int(msg[:HEADER_SIZE]) # only header from the beginning to index 10
+#             new_msg = False
+
+#         # full_msg += msg.decode("utf-8")  # decode to convert from bytes to string
+#         full_msg += msg  # don't need to decode as we want it bytes
+
+#         if len(full_msg)-HEADER_SIZE == msg_len:
+#             print("full msg recvd")
+#             # print(full_msg[HEADER_SIZE:]) # print only msg from 10 to the end
+
+#             d = pickle.loads(full_msg[HEADER_SIZE:]) # decode
+#             print(d)
+
+#             new_msg = True
+#             full_msg = b''
+
+#     print(full_msg)
